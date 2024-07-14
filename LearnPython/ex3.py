bdc382dc44b5726f1c91a3551d787e3a648714c2
tao9944 <tao9944@gmail.com>
2024-07-13 23:24:45 -0400

    adding exercise 3

---
+++ LearnPython/ex3.py
@@ -0,0 +1,38 @@
+# Print a message to about counting their chickens
+print("I will now count my chickens:")
+
+# Calculate the number of hens add 25 and 30 and divide by 6
+print("Hens", round(25 + 30 / 6, 1))
+# Calculate the number of roosters 100 minus 25 multiplied by 3 remainder of 4
+print("Roosters", round(100 - 25 * 3 % 4, 1))
+
+# Print the message about counting the eggs
+print("Now I will count the eggs:")
+
+# Calculate 4 remainder 2 and 1 divided by 4, then add 3 and 2 and 1, then
+# subtract 5 and add 0, then subtract 0.25 and add 6
+print(round(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6, 1))
+
+# Print a message asking a question about the calculation
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Calculate if 3 added to 2 is less than 5 minus 7
+print(3 + 2 < 5 - 7)
+
+# Print the question and calculate three added to 2
+print("What is 3 + 2?", 3 + 2)
+# Print the questions and calculate 5 minus 7
+print("What is 5 - 7?", 5 - 7)
+
+# Print a message about the previous calculation
+print("Oh, that's why it's False")
+
+# Print a message asking for more
+print("How about some more.")
+
+# Print a message and test if 5 is greater than -2
+print("Is it greater?", 5 > -2)
+# Print a message and test if 5 is greater than or equal to -2
+print("Is it greater or equal?", 5 >= -2)
+# Print a message and test if 5 is less than or equal to -2
+print("Is it less or equal?", 5 <= -2)
