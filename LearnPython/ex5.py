67bcc382f20a209013d7963c1c6b5249c29abaa3
tao9944 <tao9944@gmail.com>
2024-07-14 22:37:02 -0400

    add exercise 5

---
+++ LearnPython/ex5.py
@@ -0,0 +1,23 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pound heavy.")
+print("Actually that's not to heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
+
+inches = round(74 * 0.39370079, 2)
+print(f"Or {inches} inches tall.")
+kg = round(180 * 0.45359237, 2)
+print(f"Or {kg} kilograms heavy.")
