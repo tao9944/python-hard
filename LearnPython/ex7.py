7124628c617cef3ec7371d6a458c5ec512bcb510
tao9944 <tao9944@gmail.com>
2024-07-20 08:41:45 -0400

    adding exercise 7

---
+++ LearnPython/ex7.py
@@ -0,0 +1,39 @@
+# Print a message
+print("Mary had a little lamb.")
+# Print a message that includes a formatted string
+print("its fleece was white as {}.".format('snow'))
+# Print a message
+print("And everywhere that Mary went.")
+# Print a character ten times using concatenation
+print("." * 10) # what'd that do?
+
+# Create a varable and assign it a letter
+end1 = "C"
+# Create a variable and assign it a letter
+end2 = "h"
+# Create a variable and assign it a letter
+end3 = "e"
+# Create a variable and assign it a letter
+end4 = "e"
+# Create a variable and assign it a letter
+end5 = "s"
+# Create a variable and assign it a letter
+end6 = "e"
+# Create a variable and assign it a letter
+end7 = "B"
+# Create a variable and assign it a letter
+end8 = "u"
+# Create a variable and assign it a letter
+end9 = "r"
+# Create a variable and assign it a letter
+end10 = "g"
+# Create a variable and assign it a letter
+end11 = "e"
+# Create a variable and assign it a letter
+end12 = "r"
+
+# watch end = ' ' at the end. try removing it to see what happens
+# Concatenate and print variables 1 through 6 with a new space variable
+print(end1 + end2 + end3 + end4 + end5 + end6, end=" ")
+# Concatenate and print variables 7 through 12
+print(end7 + end8 + end9 + end10 + end11 + end12)
