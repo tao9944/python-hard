78a6d19aa4057c8bb2d1e5fc41b13ba936080f6f
tao9944 <tao9944@gmail.com>
2024-07-20 08:41:05 -0400

    adding exercise 6

---
+++ LearnPython/ex6.py
@@ -0,0 +1,37 @@
+# Create a variable and assign it the number 10
+types_of_people = 10
+# Create a variable and assign it a string with a variable inside
+x = f"There are {types_of_people} types of people."
+
+# Create a variable and assign it a string
+binary = "binary"
+# Create a variable and assign it another string
+do_not = "don't"
+# Create a variable and assign it a string with two variable inside it
+y = f"Those who know {binary} and those who {do_not}."
+
+# Print a variable
+print(x)
+# Print a variable
+print(y)
+
+# Print a string with a string variable inside it
+print(f"I said: {x}")
+# Print a string with a string variable inside it
+print(f"I also said: '{y}'")
+
+# Create a variable and assign it false
+hilarious = False
+# Create a variable and assign it a string and a variable
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+# Print a variable with a string variable inside it
+print(joke_evaluation.format(hilarious))
+
+# Create a variable and assign it a string
+w = "This is the left side of..."
+# Create a variable and assign it a string
+e = "a string with a right side."
+
+# Print the concatenation of two variables
+print(w + e)
