1ee1774e2bb222c0187ff2b39e54facdfc9de442
tao9944 <tao9944@gmail.com>
2024-07-22 21:56:37 -0400

    adding exercise 13

---
+++ LearnPython/ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, name, first, second, third = argv
+
+print("The script is called:", script)
+print(f"Hi {name}.")
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
