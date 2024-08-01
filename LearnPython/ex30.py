3d0ae80894eb8b697d5b5160292efa0ab6df2471
tao9944 <tao9944@gmail.com>
2024-07-31 22:51:45 -0400

    final update to exercise 30

--- LearnPython/ex30.py
+++ LearnPython/ex30.py
@@ -2,19 +2,30 @@ people = 30
 cars = 40
 trucks = 15
 
-if cars > people:
+# checks to see if cars are greater than people or trucks are
+# greater than people and if true prints the message
+if cars > people or trucks > people:
     print("We should not take the cars.")
+# this will print when the if statement is false
 else:
     print("We can't decide.")
 
-if trucks > cars:
+# checks to see if trucks are greater than cars or trucks are
+# greater than people and if true, prints the message
+if trucks > cars or trucks == people:
     print("That's too many trucks.")
+# checks to see if trucks are less than cars and if so, prints
+# the message
 elif trucks < cars:
     print("Maybe we could take the trucks.")
+# this will print if the first two statements are false
 else:
     print("We still can't decide.")
 
-if people > trucks:
+# checks to see if people are greater than trucks and cars are
+# greater than trucks and if so, prints the message
+if people > trucks and cars > trucks:
     print("Alright, let's just take the trucks.")
+# this will print if the first statement is false
 else:
     print("Fine, let's stay home then.")
