d43a2bf4307ae7571b2094be58ddf90791b31d29
tao9944 <tao9944@gmail.com>
2024-08-01 22:19:01 -0400

    adding exercise 31

---
+++ LearnPython/ex31.py
@@ -0,0 +1,14 @@
+print("""You enter a dark room with two doors/
+Do you go through door #1 or door #2?""")
+
+door = input(">")
+
+if door == "1":
+    print("There's a giant bear here eating a cheese cake.")
+    print("What do you do?")
+    print("1. Take the cake.")
+    print("2. Scream at the bear.")
+
+    bear = input(">")
+
+    
