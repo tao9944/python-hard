021915e1a1c3b493e5afcfd0da712ef5b661d128
tao9944 <tao9944@gmail.com>
2024-08-01 22:32:38 -0400

    final exercise 31

--- LearnPython/ex31.py
+++ LearnPython/ex31.py
@@ -1,5 +1,5 @@
 print("""You enter a dark room with two doors/
-Do you go through door #1 or door #2?""")
+Do you go through door #1, door #2 or door #3?""")
 
 door = input(">")
 
@@ -34,5 +34,18 @@ elif door == "2":
         print("The insanity rots your eyes into a pool of muck.")
         print("Good job!")
 
+elif door == "3":
+    print("You travel through a wormhole and travel across the galaxy.")
+    print("1. Sweet! I want to explore and boldly go.")
+    print("2. What a rip! I wanted to finish the sixth castle of Super Mario.")
+    print("3. Oh man! I'll never know if Ross and Rachel make it.")
+
+    choices = input(">")
+
+    if choices == "1" or choices == "3":
+        print("Don't worry there's still television in this part of the galaxy.")
+        print("Nice work!")
+    else:
+        print("I hope you're not out of hot pockets. You win!")
 else:
     print("You stumble around and fall on a knife and die. Good job!")
