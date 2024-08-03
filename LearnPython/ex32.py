2817779c607752e0a6e6b68b90a280555458383e
tao9944 <tao9944@gmail.com>
2024-08-03 10:33:56 -0400

    adding exercise 32

---
+++ LearnPython/ex32.py
@@ -0,0 +1,14 @@
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# this first kindo fo for-loop goes through a list
+for number in the_count:
+    print(f"This is count {number}")
+
+# same as above
+for fruit in fruits:
+    print(f"A fruit of type: {fruit}")
+
+# also we can go through mixed lists too
+# notice we have to use {} since we don't know what's in it
