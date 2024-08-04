3fabb0d168401213680a5917db2b9f79544d88f4
tao9944 <tao9944@gmail.com>
2024-08-04 09:06:32 -0400

    updating exercise 33

--- LearnPython/ex33.py
+++ LearnPython/ex33.py
@@ -1,13 +1,22 @@
 i = 0
 numbers = []
+# create a function to replace the while loop below
+def new_while_loop(stop):
+    for i in range(stop):
+        print(f"At the top i is {i}")
+        numbers.append(i)
+        print("Numbers now: ", numbers)
+        print(f"At the bottom i is {i}")
 
-while i < 6:
-  print(f"At the top i is {i}")
-  numbers.append(i)
+#while i < 6:
+  #print(f"At the top i is {i}")
+  #numbers.append(i)
 
-  i = i + 1
-  print("Numbers now: ", numbers)
-  print(f"At the bottom i is {i}")
+  #i = i + 1
+  #print("Numbers now: ", numbers)
+  #print(f"At the bottom i is {i}")
+
+new_while_loop(6)
 
 print("The numbers: ")
 
