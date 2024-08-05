2cd7d1095f701ec35ee166f4d00abeb9065991bb
tao9944 <tao9944@gmail.com>
2024-08-05 09:36:29 -0400

    adding exercise 34

---
+++ LearnPython/ex34.py
@@ -0,0 +1,36 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# the animal at 1
+animals[1]
+# the third animal
+animals[3-1]
+# the first animal
+animals[1-1]
+# the animal at 3
+animals[3]
+# the fifth animal
+animals[5-1]
+# the animal at 2
+animals[2]
+# the sixth animal
+animals[6-1]
+# the animal at 4
+animals[4]
+
+foods = ['pizza', 'cheese burger', 'tacos', 'quesadilla', 'fried chicken']
+sports = ['tennis', 'soccer', 'basketball', 'cricket', 'golf', 'baseball']
+
+# the second food
+foods[2-1]
+# the food at 0
+foods[0]
+# the food at 4
+foods[4]
+# the sport at 2
+sports[2]
+# the first sport
+sports[1-1]
+# the fourth sport
+sports[4-1]
+# the sport at 1
+sports[1]
