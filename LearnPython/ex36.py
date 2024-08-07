4e92c5b65282dc4c1db23c6efcc6d7f98a361b7f
tao9944 <tao9944@gmail.com>
2024-08-06 21:39:38 -0400

    starting exercise 36

---
+++ LearnPython/ex36.py
@@ -0,0 +1,10 @@
+# welcome message
+print("After a long walk in the forest it's nearly sunset.")
+print("The path ahead splits, leading east or west.")
+print("An owl lands in a tree next to you an gracefully hoots to the moon.")
+print("Which direction do you choose?")
+
+choice = input("> ")
+print("As you enbark on this new quest, your goal is to find you way.")
+print("Feel free to explore each place completely and discover your destiny.")
+print("In each spot you may look around, open, check your bag, take and use items.")
