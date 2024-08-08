3c6df066eb49d42d2cc52fd0f463dfb42ab0f525
tao9944 <tao9944@gmail.com>
2024-08-07 22:16:48 -0400

    update exercise 36

--- LearnPython/ex36.py
+++ LearnPython/ex36.py
@@ -1,10 +1,23 @@
-# welcome message
-print("After a long walk in the forest it's nearly sunset.")
-print("The path ahead splits, leading east or west.")
-print("An owl lands in a tree next to you an gracefully hoots to the moon.")
-print("Which direction do you choose?")
+def story():
+    # welcome message - look up how to pause between prints
+    print("After a long walk in the forest it's nearly sunset.")
+    print("The path ahead splits, leading east or west.")
+    print("An owl lands in a tree next to you an gracefully hoots to the moon.")
 
-choice = input("> ")
+    # Where should this go?
+    print("Which direction do you choose?")
+
+def menu():
+    print("Input a direction to move in that direction.")
+    print("Input 1 to look around.")
+    print("Input 2 to open something.")
+    print("Input 3 to take an item.")
+    print("Input 4 to view your inventory.")
+    print("Input 5 to use and item.")
+    choice = input("> ")
+    # check to see if input is a number
+
+# Should this be in the instructions?
 print("As you enbark on this new quest, your goal is to find you way.")
 print("Feel free to explore each place completely and discover your destiny.")
 print("In each spot you may look around, open, check your bag, take and use items.")
