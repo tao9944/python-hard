57e30e079d644c7449ee8c1be04d9bf77a5156da
tao9944 <tao9944@gmail.com>
2024-08-11 08:12:41 -0400

    updating exercise 36:

--- LearnPython/ex36.py
+++ LearnPython/ex36.py
@@ -10,27 +10,26 @@ def story():
     print("Which direction do you choose?")
 
 def menu(room):
-    print("Input a direction to move in that direction.")
-    print("Input 1 to look around.")
-    print("Input 2 to open something.")
-    print("Input 3 to take an item.")
-    print("Input 4 to view your inventory.")
-    print("Input 5 to use and item.")
-    choice = int(input("> "))
+    while True:
+        print("Which direction do you choose?")
+        choice = input("> ")
+        if choice == "east" or choice == "west":
+            break
     # check to see if input is a number/handle non-number inputs
     action(choice, room)
 
 def action(option, room):
-    print(f"You are on the {room} and chose {option}")
-
+    print(f"You are in the {room} and chose {option}")
+    # how to move to a different room
+    menu()
 def puzzle():
     print("A strange object appears and asks for your concentration.")
     puzzle_number = round(random() * 5, 0)
     if puzzle_number == 0:
         # display a number puzzle
         print("A clerk at the butcher shop is six feet tall and wears size 10 shoes. What does he weight?")
-        choice = int(input("> "))
-        if choice == "Meat" or choice == "meat":
+        choice = input("> ")
+        if "meat" in choice.lower():
             print("You are correct. How would you like to proceed?")
             points += 5
         else:
@@ -38,8 +37,8 @@ def puzzle():
     elif puzzle_number == 1:
         # display a word puzzle
         print("It's as light as a feather, but the strongest person can't hold it for five minutes. What is it?")
-        choice = (input("> ")
-        if choice == 'Breath' or choice == "breath":
+        choice = input("> ")
+        if "breath" in choice.lower():
             print("You are correct. How would you like to proceed?")
             points += 5
         else:
@@ -47,8 +46,8 @@ def puzzle():
     elif puzzle_number == 2:
         # display a logic puzzle
         print("Which is heavier, a pound of feathers or a pound of rocks?")
-        choice = (input("> ")
-        if choice == 'neither' or choice == "Neither":
+        choice = input("> ")
+        if "neither" in choice.lower():
             print("You are correct. How would you like to proceed?")
             points += 5
         else:
@@ -62,16 +61,21 @@ def puzzle():
             points += 5
         else:
             print("Sorry that's not correct.")
-    elif puzzle_number == 4:
-        # display a color puzzle
-
     else:
         # display a word puzzle
+        print("A farmer has 19 sheep on his land. One day a big storm hits and all but seven run away.")
+        print("How many sheep does the farmer have left?")
+        choice = input("> ")
+        if choice.lower() == 'seven' or choice.lower() == '7':
+            print("You are correct. How would you like to proceed?")
+            points += 5
+        else:
+            print("Sorry that's not correct.")
 story()
-
+rooms = ["path", "house", "cellar", "forest", "mountain", "cave"]
 # Should this be in the instructions?
-print("As you enbark on this new quest, your goal is to find you way.")
-print("Feel free to explore each place completely and discover your destiny.")
-print("In each spot you may look around, open, check your bag, take and use items.")
+#print("As you enbark on this new quest, your goal is to find you way.")
+#print("Feel free to explore each place completely and discover your destiny.")
+#print("In each spot you may look around, open, check your bag, take and use items.")
 
 menu("path")
