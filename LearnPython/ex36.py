090908bd88c27f057287dd09005d8a8c79296d16
tao9944 <tao9944@gmail.com>
2024-08-10 22:26:58 -0400

    update exercise 36

--- LearnPython/ex36.py
+++ LearnPython/ex36.py
@@ -1,3 +1,5 @@
+import random
+
 def story():
     # welcome message - look up how to pause between prints
     print("After a long walk in the forest it's nearly sunset.")
@@ -21,6 +23,36 @@ def menu(room):
 def action(option, room):
     print(f"You are on the {room} and chose {option}")
 
+def puzzle():
+    print("A strange object appears and asks for your concentration.")
+    puzzle_number = round(random() * 5, 0)
+    if puzzle_number == 0:
+        # display a number puzzle
+        print("What number is 3 larger than sum of 6 and 8 multiplied by 4?")
+        choice = int(input("> "))
+        if choice == 59:
+            print("You are correct. How would you like to proceed?")
+            points += 5
+        else:
+            print("Sorry that's not correct.")
+    elif puzzle_number == 1:
+        # display a word puzzle
+    elif puzzle_number == 2:
+        # display a logic puzzle
+    elif puzzle_number == 3:
+        # display a number puzzle
+        print("What number is 7 smaller than sum of 45 and 31 divided by 2?")
+        choice = int(input("> "))
+        if choice == 31:
+            print("You are correct. How would you like to proceed?")
+            points += 5
+        else:
+            print("Sorry that's not correct.")
+    elif puzzle_number == 4:
+        # display a color puzzle
+
+    else:
+        # display a word puzzle
 story()
 
 # Should this be in the instructions?
