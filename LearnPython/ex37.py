f5b60a3fc2c25167cb38707e5b23297ef449d83f
tao9944 <tao9944@gmail.com>
2024-08-14 20:44:28 -0400

    adding exercise 37

---
+++ LearnPython/ex37.py
@@ -0,0 +1,16 @@
+# logical and
+True and False == False
+
+# with-as statement
+import random as Y
+print("The random number is", Y.randint(1, 5))
+
+# assert something is true
+X = True
+assert X
+
+# stop this loop
+for x in range(1, 10):
+    if x % 3 == 0:
+        print("X equals:", x)
+        break
