f403894564dbb8cff39a1165adce894eeddda9e6
tao9944 <tao9944@gmail.com>
2024-08-17 15:20:17 -0400

    updating exercise 37

--- LearnPython/ex37.py
+++ LearnPython/ex37.py
@@ -14,3 +14,38 @@ for x in range(1, 10):
     if x % 3 == 0:
         print("X equals:", x)
         break
+
+# define a class
+class cat:
+    sound = "meow"
+
+# don't process more of the loop continue
+check = 0
+while True:
+    if check > 1:
+        print("Would you like to stop?")
+        break
+    check += 1
+    continue
+    print("This will never run:(")
+
+# define a function
+def print_helper():
+    message = input("What would you like to print? ")
+    print(message)
+
+#print_helper()
+
+# delete from dictionary
+fruits = {"1": "banana", "2": "apple", "3": "pear"}
+del fruits["2"]
+#print(fruits["2"])
+
+# braching practice
+name = "Pete"
+if name == "John":
+    print("Hi, John!")
+elif name == "Sam":
+    print("Hi, Sam!")
+else:
+    print("Hi,", name)
