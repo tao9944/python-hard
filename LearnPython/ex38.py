f8653b56077b6d1119aee9303d48722df207af2b
tao9944 <tao9944@gmail.com>
2024-08-27 22:07:04 -0400

    adding exercise 38

--- LearnPython/ex38.py
+++ LearnPython/ex38.py
@@ -21,3 +21,17 @@ print(stuff[-1]) # whoa! fancy
 print(stuff.pop())
 print(' '.join(stuff)) # what? cool!
 print('#'.join(stuff[3:5])) # super stellar!
+
+# List examples
+# 1 a player's inventory in a video game
+player_Inventory = ["sword", "torch", "knife", "bread", "potion",
+    "compass", "map", "shield", "meat"] 
+# 2 the items in a person's to do List
+# 3 the baseball cards in a collection
+# 4 a mechanic's tools
+# 5 an artist's paintings
+# 6 the inventory of a small business
+# 7 an investor's portfolio
+# 8 a library's books
+# 9 family recipes
+# 10 vacation bucket list
