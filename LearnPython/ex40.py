c4dd6a0832b33b5f6d9b4b3c56d19cab46c92d0c
tao9944 <tao9944@gmail.com>
2024-09-11 22:46:45 -0400

    updating exercise 40

--- LearnPython/ex40.py
+++ LearnPython/ex40.py
@@ -10,3 +10,17 @@ class Song(object):
 happy_bday = Song(["Happy birthday to you",
                     "I don't want to get sued",
                     "So I'll stop right there"])
+
+bulls_on_parade = Song(["They rally around tha family",
+                        "With pockets full of shells"])
+
+ode_to_joy = Song(["Joy, beautiful spark of Divinity",
+                    "We enter, drunk with fire"])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
+
+ode_to_joy.sing_me_a_song()
+
+new_words = "On top of old smokey, all covered in cheese"
