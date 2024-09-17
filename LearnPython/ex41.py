3a44a0a3dfc9971bbc09ef23604bbf5a432c48cd
tao9944 <tao9944@gmail.com>
2024-09-16 20:44:41 -0400

    Adding exercise 41

---
+++ LearnPython/ex41.py
@@ -0,0 +1,23 @@
+import random
+from urllib.request import urlopen
+import sys
+
+WORD_URL = "http://learncodethehardway.org/words.txt"
+WORDS = []
+
+PHRASES = {
+    "class %%%(%%%)":
+        "Make a class named %%% that is-a %%%.",
+    "class %%%(object):\n\tdef __init__(self, ***)" :
+        "class %%% has-a __init__ that takes self and *** params.",
+    "class %%%(object):\n\tdef ***(self, @@@)":
+        "class %%% has-a function *** that takes self and @@@ params.",
+    "*** = %%%()":
+        "Set *** to an instance of class %%%.",
+    "***.***(@@@)":
+        "From *** get the *** function, call it with params self, @@@.",
+    "***.*** = '***'*":
+        "From *** get the *** atribute and set it to '***'."
+}
+
+# do they want to drill phrases first
